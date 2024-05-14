973be05f2cff2f6d8d387cb4473b3772b7d61d62
Kenneth Estanislao <hacksider@gmail.com>
2024-05-14 10:40:06 +0800

    Update run_with_chocolatey.bat
    added ffmpeg installation on chocolatey

--- run_with_chocolatey.bat
+++ run_with_chocolatey.bat
@@ -3,6 +3,8 @@
 	choco install vcredist-all
 	:: Installing CUDA if it's not already installed
 	choco install cuda
+	:: Inatalling ffmpeg if it's not already installed
+	choco install ffmpeg
 	:: Installing Python if it's not already installed
 	choco install python -y
 	:: Assuming successful installation, we ensure pip is upgraded
