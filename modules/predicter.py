408b0f4cf7714982945bc5a42d49a376029c32ba
Vic P <vic4key@gmail.com>
2024-08-13 04:16:34 +0700

    ## Fix & Improve the NSFW function - Fixed incorrect state usage. - Removed the redundant argument that caused exceptions. - Prevented the app from closing when an image is flagged as NSFW.

--- modules/predicter.py
+++ modules/predicter.py
@@ -6,9 +6,11 @@ from modules.typing import Frame
 MAX_PROBABILITY = 0.85
 
 # Preload the model once for efficiency
-model = opennsfw2.make_open_nsfw_model()
+model = None
 
 def predict_frame(target_frame: Frame) -> bool:
+    global model
+    if model is None: model = opennsfw2.make_open_nsfw_model()
     image = Image.fromarray(target_frame)
     image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = np.expand_dims(image, axis=0)
@@ -16,9 +18,9 @@ def predict_frame(target_frame: Frame) -> bool:
     return probability > MAX_PROBABILITY
 
 def predict_image(target_path: str) -> bool:
-    probability = opennsfw2.predict_image(target_path, model=model)
+    probability = opennsfw2.predict_image(target_path)
     return probability > MAX_PROBABILITY
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100, model=model)
+    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return any(probability > MAX_PROBABILITY for probability in probabilities)
