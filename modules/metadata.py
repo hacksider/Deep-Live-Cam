5cddab91554f053a966fa89c26522e8d70dc00ae
Tay754 <t7547347@gmail.com>
2024-08-13 05:24:53 -0400

    Delete modules directory
    Fraud

--- modules/metadata.py
+++
@@ -1,3 +0,0 @@
-name = 'Deep Live Cam'
-version = '1.3.0'
-edition = 'Portable'
--- modules/typing.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-from insightface.app.common import Face
-import numpy
-
-Face = Face
-Frame = numpy.ndarray[Any, Any]
