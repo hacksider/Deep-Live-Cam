673439d47c02c23f6cfde65ecb25b387365af966
KRSHH <136873090+KRSHH@users.noreply.github.com>
2024-10-09 19:50:20 +0530

    Update globals.py for Default states

--- modules/globals.py
+++ modules/globals.py
@@ -2,11 +2,11 @@ import os
 from typing import List, Dict, Any
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-WORKFLOW_DIR = os.path.join(ROOT_DIR, 'workflow')
+WORKFLOW_DIR = os.path.join(ROOT_DIR, "workflow")
 
 file_types = [
-    ('Image', ('*.png','*.jpg','*.jpeg','*.gif','*.bmp')),
-    ('Video', ('*.mp4','*.mkv'))
+    ("Image", ("*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp")),
+    ("Video", ("*.mp4", "*.mkv")),
 ]
 
 souce_target_map = []
@@ -16,23 +16,23 @@ source_path = None
 target_path = None
 output_path = None
 frame_processors: List[str] = []
-keep_fps = None
-keep_audio = None
-keep_frames = None
-many_faces = None
-map_faces = None
-color_correction = None  # New global variable for color correction toggle
-nsfw_filter = None
+keep_fps = True
+keep_audio = True
+keep_frames = False
+many_faces = False
+map_faces = False
+color_correction = False  # New global variable for color correction toggle
+nsfw_filter = False
 video_encoder = None
 video_quality = None
-live_mirror = None
-live_resizable = None
+live_mirror = False
+live_resizable = False
 max_memory = None
 execution_providers: List[str] = []
 execution_threads = None
 headless = None
-log_level = 'error'
-fp_ui: Dict[str, bool] = {}
+log_level = "error"
+fp_ui: Dict[str, bool] = {"face_enhancer": False}
 camera_input_combobox = None
 webcam_preview_running = False
 show_fps = False
