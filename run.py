923baeadda8c0ae899b7ec4038e1ed633432445d
bumprat <bumprat@users.noreply.github.com>
2025-05-03 13:33:36 +0800

    Update run.py
    make the program compatible with python embedded intepreter

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from modules import core
+import sys
+import os
+
+# python embedded support: Ensure the script's folder is in sys.path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
     core.run()
