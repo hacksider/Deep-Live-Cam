fe4a87e8f23a9bbd9dd92517f5dac64668ef31f5
KUNJ SHAH <kunjshah572005@gmail.com>
2025-05-05 13:19:29 +0000

    update

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,26 +2,17 @@ import os
 import cv2
 import numpy as np
 
-# Define a new function that supports unicode characters in file paths
+# Utility function to support unicode characters in file paths for reading
 def imread_unicode(path, flags=cv2.IMREAD_COLOR):
     return cv2.imdecode(np.fromfile(path, dtype=np.uint8), flags)
 
-# Override the original `cv2.imread`
-cv2.imread = imread_unicode
-
-# Define a function to support unicode characters in file paths when saving
+# Utility function to support unicode characters in file paths for writing
 def imwrite_unicode(path, img, params=None):
-    # Encode the image
     root, ext = os.path.splitext(path)
-    # If no extension is found, you can choose a default extension (e.g., ".png")
     if not ext:
         ext = ".png"
     result, encoded_img = cv2.imencode(ext, img, params if params else [])
-
     if result:
-        encoded_img.tofile(path)  # Save image using numpy's `tofile()`
+        encoded_img.tofile(path)
         return True
     return False
-
-# Override `cv2.imwrite`
-cv2.imwrite = imwrite_unicode
